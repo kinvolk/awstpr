3ec9aacd0e72b2b4dc61fc3bde05947629a0b0c7
Ross Fairbanks <rossf7@users.noreply.github.com>
2017-04-11 12:26:23 +0200

    Remove KMSKeyARN as key is auto generated per cluster (#16)

--- aws/aws.go
+++ aws/aws.go
@@ -1,9 +1,8 @@
 package aws
 
 type AWS struct {
-	KMSKeyArn string `json:"kmsKeyArn" yaml:"kmsKeyArn"`
-	Masters   []Node `json:"masters" yaml:"masters"`
-	Region    string `json:"region" yaml:"region"`
-	AZ        string `json:"az" yaml:"az"`
-	Workers   []Node `json:"workers" yaml:"workers"`
+	Masters []Node `json:"masters" yaml:"masters"`
+	Region  string `json:"region" yaml:"region"`
+	AZ      string `json:"az" yaml:"az"`
+	Workers []Node `json:"workers" yaml:"workers"`
 }
